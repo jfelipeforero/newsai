4ca16e560afbecf3796b5c07674e676a138027e9
androusf <andresfrw@hotmail.com>
2024-05-03 13:47:15 -0500

    footer

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -21,11 +21,10 @@ import { sendTextToBackend } from './services/api';
 
 const App = () => {
   const [response, setResponse] = useState('');
-  const [darkMode, setDarkMode] = useState(false);
 
   const handleSubmit = async (text) => {
     // Simulación de respuesta del backend 
-    const fakeBackendResponse = `esta noticia es rial o falsa qn sabe "${text}"`;
+    const fakeBackendResponse = `la noticia es fake "${text}"`;
     
     // Actualiza el estado con la respuesta simulada del backend
     setResponse(fakeBackendResponse);
@@ -34,9 +33,16 @@ const App = () => {
   return (
     <div className='container background'>
       <div className='content'>
-        <h1 className='title'>NewSai</h1>
+        <h1 className='title'>Newsai</h1>
+        <h2 className='subtitle'>Are your news reliable? Find out here!</h2>
         <TextInput onSubmit={handleSubmit} />
         <ResponseBox response={response} />
+        <p className="footer-text">
+          Developed by: 
+          <a href="https://www.linkedin.com/in/andresfeliperocha/"target='_blank'>Andres Rocha</a>, 
+          <a href="https://www.linkedin.com/in/jfelipeforero/"target='_blank'>Felipe Forero</a>, 
+          <a href="https://www.linkedin.com/in/marlon-calle-areiza/"target='_blank'>Marlon Calle</a>
+        </p>
       </div>
     </div>
   );
