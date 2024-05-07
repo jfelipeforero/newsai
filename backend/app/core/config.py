78e0ff4ba051f5d9e6a9eb3c6319623d102999fd
Marlondot <mcallea@unal.edu.co>
2024-05-06 19:45:06 -0500

    Fixed: Libraries version in requirements and a comment left uncommented

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,12 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     google_api_key: str = ""
-<<<<<<< HEAD
-    lambda_url: str= ""
-=======
     lambda_url: str = ""
 
->>>>>>> a529ac30160cc40fe75fac9b8ff2395d59536145
     model_config = SettingsConfigDict(env_file=".env")
 
 
